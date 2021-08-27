76dc7ef80bc4d205ed36fa805dbc09776814c322
Ann Michelle Gonzales <ann.michelle.gonzales112@gmail.com>
2021-08-27 12:09:41 -0500

    build

---
+++ leadmanager/frontend/src/components/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react'
+
+class Apps extends React.Component {
+    render() {
+        return <h1>React App</h1>
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
