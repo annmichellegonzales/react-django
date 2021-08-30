4197705a53dfd1125c61c1cf98cbdf44a38bd8d9
Ann Michelle Gonzales <ann.michelle.gonzales112@gmail.com>
2021-08-29 20:22:39 -0500

    main.js dev

--- leadmanager/frontend/urls.py
+++ leadmanager/frontend/urls.py
@@ -1,5 +1,7 @@
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
+
 urlpatterns = [
-  path('', views.index)
+  path('', include('frontend.urls')),
+  path('', include('leads.urls'))
 ]
