fff5601ebf7b2b40af5090506cb07dabfe55b512
Ann Michelle Gonzales <ann.michelle.gonzales112@gmail.com>
2021-08-29 19:57:52 -0500

    updated view

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
-module.export = {
+module.exports = {
     module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            }
-        ]
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
+        }
+      ]
     }
-}
+  }
